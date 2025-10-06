287bd013b01343751a29a7cd3567003168e3ae56
sarahraquely <sara.lourenco.oliveira@escola.pr.gov.br>
2025-10-06 08:35:25 -0300

    first commit

---
+++ service-worker.js
@@ -0,0 +1,10 @@
+self.addEventListener("install", event => {
+    console.log("Service Worker instalado");
+  });
+  
+  self.addEventListener("activate", event => {
+    console.log("Service Worker ativado");
+  });
+  
+  self.addEventListener("fetch", event => {
+  });
